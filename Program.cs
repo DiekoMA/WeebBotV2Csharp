977a1399ae4624637954296d0a6f07ac08a54649
Diekoloreoluwa Mayowa Akinsete <dj_blueewastaken@outlook.com>
2026-04-26 21:48:23 +0300

    changes

--- Program.cs
+++ Program.cs
@@ -1,16 +1,47 @@
 ﻿using Microsoft.Extensions.Hosting;
+using NetCord;
+using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
 using NetCord.Hosting.Services;
 using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Logging;
+using NetCord.Rest;
+using NetCord.Services;
+using NetCord.Services.ApplicationCommands;
 
 
-var builder = Host.CreateApplicationBuilder(args);
+    
+GatewayClient client;
+string token = Environment.GetEnvironmentVariable("BotToken") ?? "";
 
-        builder.Services
-            .AddDiscordGateway()
-            .AddApplicationCommands();
+client = new GatewayClient(new BotToken(token), new GatewayClientConfiguration
+{
+    Logger = new ConsoleLogger(),
+    Intents = GatewayIntents.All,
+});
 
-        var host = builder.Build();
+ApplicationCommandService<ApplicationCommandContext> applicationCommandService = new ();
 
-        host.AddModules(typeof(Program).Assembly);
-        await host.RunAsync();
+applicationCommandService.AddModules(typeof(Program).Assembly);
+
+client.InteractionCreate += async interaction =>
+{
+    if (interaction is not ApplicationCommandInteraction applicationCommandInteraction)
+        return;
+
+    var result = await applicationCommandService.ExecuteAsync(new ApplicationCommandContext(applicationCommandInteraction, client));
+
+
+    if (result is not IFailResult failResult) 
+        return;
+
+    try
+    {
+        await interaction.SendResponseAsync(InteractionCallback.Message(failResult.Message));
+    } catch {}
+};
+
+await applicationCommandService.RegisterCommandsAsync(client.Rest, client.Id);
+
+await client.StartAsync();
+await Task.Delay(-1);
