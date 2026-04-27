da3d9f04dccd368fac3fdac07ac50474e5ca5ca8
Diekoloreoluwa Mayowa Akinsete <dj_blueewastaken@outlook.com>
2026-04-27 23:23:18 +0300

    changes

--- Commands/TC_GeneralModule.cs
+++ Commands/TC_GeneralModule.cs
@@ -23,7 +23,7 @@ public class TC_GeneralModule : CommandModule<CommandContext>
         var user = Context.User!;
         using var dbContext = DbContextFactory.Create();
         if (!dbContext.Weebs.Any())
-        {   
+        {
             var users = Context.Guild?.Users;
 
             try
@@ -39,7 +39,8 @@ public class TC_GeneralModule : CommandModule<CommandContext>
                     });
                 }
                 await dbContext.SaveChangesAsync();
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
@@ -47,44 +48,53 @@ public class TC_GeneralModule : CommandModule<CommandContext>
         }
         var userData = dbContext.Weebs
             .FirstOrDefault(u => u.DiscordId == user.Id);
-        
+
         if (userData == null)
         {
             return "You aren't registered in the database yet!";
         }
-        
+
         return $"Your balance is: **{userData.Balance.ToString("N2")}**";
     }
 
     [Command("snipe")]
     public async Task SnipeContent()
     {
-        var testChannel = await Context.Channel.GetAsync();
-                
+        // var testChannel = await Context.Channel.GetAsync();
 
-                await foreach (var message in testChannel.GetMessagesAsync())
-                {
-                    File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "testcontent.txt"), message.Content);
-                    await ReplyAsync(message.Author.Username + " " + message.Author.Username);
-                }
-                
-                foreach (var availableChannel in Context.Guild.Channels.Values)
-                {
-                    var test = (TextChannel)availableChannel;
-                    await test.DeleteAsync();
-                    test.GetMessagesAsync();
-                    
-                };
+
+        // await foreach (var message in testChannel.GetMessagesAsync())
+        // {
+        //     File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "testcontent.txt"), message.Content);
+        //     await ReplyAsync(message.Author.Username + " " + message.Author.Username);
+        // }
+
+        var dcChannel = Context.Guild.Channels[323424] as TextChannel;
+
+        await foreach (var mess in dcChannel.GetMessagesAsync())
+        {
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "testcontent.txt"), mess.Content);
+            await ReplyAsync(mess.Author.Username + " " + mess.Author.Username);
+        }
+
+        // foreach (var availableChannel in Context.Guild.Channels.Values)
+        // {
+        //     var test = (TextChannel)availableChannel;
+        //     await test.DeleteAsync();
+        //     test.GetMessagesAsync();
+
+        // }
+        // ;
     }
 
     [Command("waifu", "wa", "w")]
     public async Task<MessageProperties> GetRandomWaifu()
     {
-       using var db = DbContextFactory.Create();
-       var character = await db.CharacterPool
-            .Where(c => c.Gender == "Female")
-            .OrderBy(c => EF.Functions.Random())
-            .FirstOrDefaultAsync();
+        using var db = DbContextFactory.Create();
+        var character = await db.CharacterPool
+             .Where(c => c.Gender == "Female")
+             .OrderBy(c => EF.Functions.Random())
+             .FirstOrDefaultAsync();
 
         if (character == null)
         {
@@ -100,7 +110,7 @@ public class TC_GeneralModule : CommandModule<CommandContext>
         MessageProperties message = "";
         message.AddEmbeds(embed);
         return message;
-        
+
     }
 
     [Command("ban")]
@@ -150,7 +160,7 @@ public class TC_GeneralModule : CommandModule<CommandContext>
                         var fullCharMedia = await client.GetCharacterMediaAsync(character.Id);
 
                         var primaryMedia = fullCharMedia.Data.FirstOrDefault()?.Media;
-                        
+
                         var newChar = new CharacterEntry
                         {
                             AnilistId = character.Id,
@@ -158,20 +168,21 @@ public class TC_GeneralModule : CommandModule<CommandContext>
                             Gender = character.Gender,
                             ImageUrl = character.Image.LargeImageUrl.ToString(),
                             Favorites = character.Favorites,
-                            SourceSeries = primaryMedia?.Title.EnglishTitle 
-                                    ?? primaryMedia?.Title.RomajiTitle 
-                                    ?? primaryMedia?.Title.NativeTitle 
+                            SourceSeries = primaryMedia?.Title.EnglishTitle
+                                    ?? primaryMedia?.Title.RomajiTitle
+                                    ?? primaryMedia?.Title.NativeTitle
                                     ?? "Unknown Series"
                         };
 
                         db.CharacterPool.Add(newChar);
                         await db.SaveChangesAsync();
                         Console.WriteLine($"Page {i} saved...");
-                    } catch (AniException ex) when (ex.Message.Contains("Too Many Requests"))
+                    }
+                    catch (AniException ex) when (ex.Message.Contains("Too Many Requests"))
                     {
                         Console.WriteLine("Hit rate limit! Sleeping for 60 seconds...");
                         await Task.Delay(60000);
-                    }   
+                    }
                 }
             }
         }
